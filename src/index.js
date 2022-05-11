cc4c632dc1f0d44fca3dc252b0aaca9e88770796
Piron Evgeniy <piron.johny@gmail.com>
2022-05-11 20:07:06 +0000

    Fix route basename

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter>
+        <BrowserRouter basename="goit-react-hw-08-phonebook">
           <App />
         </BrowserRouter>
       </PersistGate>
